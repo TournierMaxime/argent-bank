0ab18d4e28c51bb94e2735afaca43fb89ca461fb
Maxime Tournier <mtournier@devsdefrance.com>
2024-08-22 15:44:19 +0200

    Account data + update user comp

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,47 +1,60 @@
-import React, { Fragment } from "react"
+import React, { Fragment, useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import useUserUpdate from "../hooks/useUserUpdate"
+import { oneUser } from "../redux/actions/user"
 
 const Account = () => {
+  const dispatch = useDispatch()
+
+  const token = useSelector((state) => state.login.data.token)
+  const accountData = useSelector((state) => state.login.data.accountData)
+  const userData = useSelector((state) => state.oneUser.data)
+
+  console.log(token)
+
+  const firstName = userData?.firstName
+  const lastName = userData?.lastName
+
+  const { handleUpdateUser } = useUserUpdate({ data: userData })
+
+  const fetchData = () => {
+    return (
+      accountData &&
+      accountData.map((data, index) => {
+        return (
+          <section key={index} className="account">
+            <div className="account-content-wrapper">
+              <h3 className="account-title">{data.title}</h3>
+              <p className="account-amount">{data.amount}</p>
+              <p className="account-amount-description">{data.description}</p>
+            </div>
+            <div className="account-content-wrapper cta">
+              <button className="transaction-button">View transactions</button>
+            </div>
+          </section>
+        )
+      })
+    )
+  }
+
+  useEffect(() => {
+    dispatch(oneUser(token))
+  }, [token])
+
   return (
     <Fragment>
       <div className="header">
         <h1>
           Welcome back
           <br />
-          Tony Jarvis!
+          {firstName} {lastName}!
         </h1>
-        <button className="edit-button">Edit Name</button>
+        <button className="edit-button" onClick={() => handleUpdateUser()}>
+          Edit Name
+        </button>
       </div>
       <h2 className="sr-only">Accounts</h2>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-          <p className="account-amount">$2,082.79</p>
-          <p className="account-amount-description">Available Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <button className="transaction-button">View transactions</button>
-        </div>
-      </section>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-          <p className="account-amount">$10,928.42</p>
-          <p className="account-amount-description">Available Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <button className="transaction-button">View transactions</button>
-        </div>
-      </section>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-          <p className="account-amount">$184.30</p>
-          <p className="account-amount-description">Current Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <button className="transaction-button">View transactions</button>
-        </div>
-      </section>
+      {fetchData()}
     </Fragment>
   )
 }
